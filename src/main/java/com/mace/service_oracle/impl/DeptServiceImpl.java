b889e938d5386bdc99b98d433918cd09de19b71d
mace <1525752742@qq.com>
2018-06-02 12:04:57 +0800

    解决oracle bug

--- src/main/java/com/mace/service_oracle/impl/DeptServiceImpl.java
+++ src/main/java/com/mace/service_oracle/impl/DeptServiceImpl.java
@@ -4,6 +4,7 @@ import com.mace.common.ResponseMessage;
 import com.mace.entity.Dept;
 import com.mace.mapper_oracle.DeptMapper;
 import com.mace.service_oracle.IDeptService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
  * Created by mace on 11:30 2018/5/30.
  */
 @Service("iDeptService")
+@Slf4j
 public class DeptServiceImpl implements IDeptService {
 
     @Autowired
@@ -24,6 +26,8 @@ public class DeptServiceImpl implements IDeptService {
         if(deptMapper.insert(dept) == 0){
             return ResponseMessage.createByErrorMessage("注册部门信息失败");
         }
+
+        log.info("oracle insert返回主键值 id: {}",dept.getId());
         return ResponseMessage.createBySuccessMessage("注册部门信息成功");
     }
 }
