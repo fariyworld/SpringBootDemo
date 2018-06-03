a863f1d9ade56e9848cddcd83a065c63a88f05ad
mace <1525752742@qq.com>
2018-06-03 11:40:18 +0800

    @PathVariable

--- src/main/java/com/mace/controller/mongodb/MongoController.java
+++ src/main/java/com/mace/controller/mongodb/MongoController.java
@@ -4,6 +4,7 @@ import com.mace.common.ResponseMessage;
 import com.mace.mongodb.service.IMongoService;
 import com.mace.service.mysql.IUserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,15 +21,14 @@ import java.lang.annotation.Repeatable;
 @Slf4j
 public class MongoController {
 
-
+    @Autowired
     private IMongoService iMongoService;
+    @Autowired
     private IUserService iUserService;
 
 
     @RequestMapping(value = "createCollection/{collectionName}")
     public ResponseMessage<String> createCollection(@PathVariable("collectionName") String collectionName){
-
-        log.info("要创建的表名：{}",collectionName);
 
         if(iMongoService.createCollection(collectionName))
             return ResponseMessage.createBySuccessMessage("创建" +collectionName+ " 成功");
@@ -39,8 +39,6 @@ public class MongoController {
 
     @RequestMapping(value = "importData/{collectionName}")
     public ResponseMessage<String> importData(@PathVariable("collectionName") String collectionName){
-
-        log.info("要导入数据的表名：{}",collectionName);
 
         if(iMongoService.insert(iUserService.findAll(), collectionName))
             return ResponseMessage.createBySuccessMessage("导入数据至" +collectionName+ " 成功");
